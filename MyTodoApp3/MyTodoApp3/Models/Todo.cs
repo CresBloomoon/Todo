1b1d14d41aa6e70e83faf381bc9e897b423056ff
CresBloomoon <metroidm2421@gmail.com>
2023-03-05 21:54:30 +0900

    Todo追加ウィンドウ追加

---
+++ MyTodoApp3/MyTodoApp3/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTodoApp3.Models
+{
+    public sealed class Todo
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreationTime { get; set; }
+
+    }
+}
---
+++ MyTodoApp3/MyTodoApp3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Livet;
+using Livet.Commands;
+using Livet.EventListeners;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.Messaging.Windows;
+using MyTodoApp3.Models;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MyTodoApp3.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public ObservableCollection<Todo> Todos { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            Todos = new ObservableCollection<Todo>
+            {
+                new Todo() { Title = "test1", IsDone = false, CreationTime= DateTime.Now },
+                new Todo() { Title = "test2", IsDone = false, CreationTime= DateTime.Now },
+                new Todo() { Title = "test2", IsDone = false, CreationTime= DateTime.Now },
+            };
+        }
+    }
+}
