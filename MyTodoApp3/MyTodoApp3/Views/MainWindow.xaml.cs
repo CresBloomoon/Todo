670f4e3a7d23bb8db8fd0fac042bad5fc0c25cc9
CresBloomoon <metroidm2421@gmail.com>
2023-03-13 21:54:50 +0900

    追加機能を実装しました

--- MyTodoApp3/MyTodoApp3/Views/MainWindow.xaml.cs
+++ MyTodoApp3/MyTodoApp3/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MyTodoApp3.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -25,30 +26,39 @@ namespace MyTodoApp3.Views
      */
     public partial class MainWindow : Window
     {
+        ObservableCollection<Todo> todoList = new ObservableCollection<Todo>();
+        MainWindowViewModel vm = new MainWindowViewModel();
         public MainWindow()
         {
             InitializeComponent();
 
-            var vm = new MainWindowViewModel();
-            vm.Todos.Add(new Todo()
-            {
-                Title = "test1",
-                IsDone = true,
-                CreationTime = DateTime.Now
-            });
+            //var vm = new MainWindowViewModel();
+            //vm.Todos.Add(new Todo()
+            //{
+            //    Title = "test1",
+            //    IsDone = true,
+            //    CreationTime = DateTime.Now
+            //});
 
-            vm.Todos.Add(new Todo()
-            {
-                Title = "test1",
-                IsDone = true,
-                CreationTime = DateTime.Now
-            });
+            //vm.Todos.Add(new Todo()
+            //{
+            //    Title = "test2",
+            //    IsDone = true,
+            //    CreationTime = DateTime.Now
+            //});
 
             this.DataContext = vm;
+            //this.DataContext = todoList;
+        }
+
+        private void MainWIndow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
         }
 
         /// <summary>
-        /// 項目を削除
+        /// 項目を削除します
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -58,5 +68,29 @@ namespace MyTodoApp3.Views
             if (TodoListBox.SelectedItem == null) return;
             TodoListBox.Items.Remove(TodoListBox.SelectedItem);
         }
+
+        /// <summary>
+        /// 項目を追加します
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            //テキストボックスに何も書かれていない場合はなにもしない
+            if (InputTextBox.Text == null) return;
+
+            //var vm = new MainWindowViewModel();
+            vm.Todos.Add(new Todo()
+            {
+                Title = InputTextBox.Text,
+                CreationTime = DateTime.Now,
+                IsDone = false,
+            });
+
+            //InputTextBox内のテキストを削除
+            InputTextBox.Text = null;
+        }
+
+
     }
 }
